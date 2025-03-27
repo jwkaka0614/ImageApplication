e6b8d4f3185b317f6949533cfbbed4f8c03d766e
jwkaka0614 <work24661483@gmail.com>
2025-03-27 17:14:07 +0800

    新增fetchFolderPath

--- app/src/main/java/com/example/imageapplication/ImageViewModel.kt
+++ app/src/main/java/com/example/imageapplication/ImageViewModel.kt
@@ -18,14 +18,17 @@ class ImageViewModel @Inject constructor(
     private val imageManager: ImageManager, //圖片
     private val folderRepository: FolderRepository //資料夾
 ) : ViewModel() {
-
+    private var _currentPath = MutableStateFlow("")
+    private val currentPath = _currentPath.asStateFlow()
     private val _imageList = MutableStateFlow<List<ImageModel>>(emptyList())
     val imageList = _imageList.asStateFlow()
+    private val _folderList = MutableStateFlow<List<String>>(emptyList())
+    val folderList = _folderList.asStateFlow()
 //    init {
 //        imageManager.getImageList()
 //    }
 
-    fun fetchImages() {
+    fun fetchImages(path: String = "") {
         viewModelScope.launch {
             imageManager.getImageFlow().collect { image ->
                 // 每讀到一筆資料就追加到現有列表中
@@ -35,4 +38,11 @@ class ImageViewModel @Inject constructor(
             }
         }
     }
+    fun fetchFolderPath() {
+        val folderGrouping = folderRepository.getFirstLevel(_imageList.value)
+        _folderList.value = folderGrouping.subFolders.map { it.folderName }
+        _currentPath.value = folderGrouping.rootFolder
+    }
+
+
 }
