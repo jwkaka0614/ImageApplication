ac1d03eb1e3e464208d043d9aaa3b39da75d5a3b
jwkaka0614 <work24661483@gmail.com>
2025-03-27 12:27:05 +0800

    modified name and singleton

--- app/src/main/java/com/example/imageapplication/module/ViewModelFactory.kt
+++ app/src/main/java/com/example/imageapplication/module/ViewModelFactory.kt
@@ -2,14 +2,12 @@ package com.example.imageapplication.module
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import dagger.MapKey
 import javax.inject.Inject
 import javax.inject.Provider
-import kotlin.reflect.KClass
+import javax.inject.Singleton
 
-
-
-class DaggerViewModelFactory @Inject constructor(
+@Singleton
+class ViewModelFactory @Inject constructor(
     private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
