abb1bf4313c87fc09c2303b07e66a31d5cc550fa
jwkaka0614 <work24661483@gmail.com>
2025-03-27 17:14:22 +0800

    modifier

--- app/src/main/java/com/example/imageapplication/model/Model.kt
+++ app/src/main/java/com/example/imageapplication/model/Model.kt
@@ -2,6 +2,14 @@ package com.example.imageapplication.model
 
 import android.net.Uri
 
-data class FoldModel(val folderPath: String, val folderName: String)
+data class FoldModel(
+    val folderPath: String,
+    val folderName: String
+)
+
+data class FolderGrouping(
+    val rootFolder: String,       // 全局根目錄，例如 "/rootPath/"
+    val subFolders: List<FoldModel>  // 子資料夾列表
+)
 
 data class ImageModel(val uri: Uri, val name: String, val folderPath: String)
