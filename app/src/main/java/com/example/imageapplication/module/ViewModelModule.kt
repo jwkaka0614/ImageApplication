e7b41404c5a26e4a69893a674fb1c46740e1e378
jwkaka0614 <work24661483@gmail.com>
2025-03-27 12:27:37 +0800

    modified DI ViewModelFactory

--- app/src/main/java/com/example/imageapplication/module/ViewModelModule.kt
+++ app/src/main/java/com/example/imageapplication/module/ViewModelModule.kt
@@ -1,6 +1,7 @@
 package com.example.imageapplication.module
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.imageapplication.ImageViewModel
 import dagger.Binds
 import dagger.Module
@@ -8,6 +9,10 @@ import dagger.multibindings.IntoMap
 
 @Module
 abstract class ViewModelModule {
+
+    @Binds
+    abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+
     @Binds
     @IntoMap
     @ViewModelKey(ImageViewModel::class)
