8fbcd7ba326155f6361e6dfb67af0267cccbda87
jwkaka0614 <work24661483@gmail.com>
2025-03-28 11:43:16 +0800

    避免currentPath初始化重複呼叫

--- app/src/main/java/com/example/imageapplication/ImageViewModel.kt
+++ app/src/main/java/com/example/imageapplication/ImageViewModel.kt
@@ -1,15 +1,14 @@
 package com.example.imageapplication
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.imageapplication.manager.FolderRepository
 import com.example.imageapplication.manager.ImageManager
+import com.example.imageapplication.model.FolderModel
 import com.example.imageapplication.model.ImageModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.drop
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -19,16 +18,16 @@ class ImageViewModel @Inject constructor(
     private val folderRepository: FolderRepository //資料夾
 ) : ViewModel() {
     private var _currentPath = MutableStateFlow("")
-    private val currentPath = _currentPath.asStateFlow()
+    val currentPath = _currentPath.asStateFlow()
     private val _imageList = MutableStateFlow<List<ImageModel>>(emptyList())
     val imageList = _imageList.asStateFlow()
-    private val _folderList = MutableStateFlow<List<String>>(emptyList())
+    private val _folderList = MutableStateFlow<List<FolderModel>>(emptyList())
     val folderList = _folderList.asStateFlow()
 
     init {
         // 監聽 currentPath 的變化，調用 fetchImages 新操作
         viewModelScope.launch {
-            currentPath.collect { newPath ->
+            currentPath.drop(1).collect { newPath ->
                 // 在 currentPath 變化後，先清空目前圖片列表（可選）
                 _imageList.value = emptyList()
                 // 根據新的路徑讀取圖片
@@ -39,7 +38,7 @@ class ImageViewModel @Inject constructor(
 
     fun fetchImages(path: String = "") {
         viewModelScope.launch {
-            imageManager.getImageFlow().collect { image ->
+            imageManager.getImageFlow(path).collect { image ->
                 // 每讀到一筆資料就追加到現有列表中
                 _imageList.update { currentList ->
                     currentList + image
@@ -47,10 +46,15 @@ class ImageViewModel @Inject constructor(
             }
         }
     }
+
     fun fetchFolderPath() {
         val folderGrouping = folderRepository.getFirstLevel(_imageList.value)
-        _folderList.value = folderGrouping.subFolders.map { it.folderName }
+        _folderList.value = folderGrouping.subFolders.map { it }
         _currentPath.value = folderGrouping.rootFolder
+    }
+
+    fun updateCurrentPath(newPath: String) {
+        _currentPath.value = newPath
     }
 
 
