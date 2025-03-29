baba8161bd22851210e0f60d69eaa7c07004f7f8
jwkaka0614 <work24661483@gmail.com>
2025-03-29 22:55:15 +0800

    Modified

--- app/src/main/java/com/example/imageapplication/module/AppModule.kt
+++ app/src/main/java/com/example/imageapplication/module/AppModule.kt
@@ -5,20 +5,25 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import com.example.imageapplication.ImageViewModel
 import com.example.imageapplication.manager.FolderRepository
+import com.example.imageapplication.manager.IImageManager
 import com.example.imageapplication.manager.ImageManager
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoMap
+import javax.inject.Singleton
 
 @Module
 class AppModule {
     @Provides
+    @Singleton
     fun provideApplicationContext(application: Application): Context = application.applicationContext
 
     @Provides
-    fun provideImageManager(context: Context): ImageManager = ImageManager(context)
+    @Singleton
+    fun provideImageManager(context: Context): IImageManager = ImageManager(context)
 
     @Provides
+    @Singleton
     fun provideFolderRepository(context: Context): FolderRepository = FolderRepository(context)
 }
